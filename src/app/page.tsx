a7e00d6fa8e2744e56d4916a9610911f0a1052b5
anonthedev <opss59332@gmail.com>
2025-07-15 22:00:04 +0530

    random home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+
 export default function Home() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) {
+      router.push("/dashboard");
+    } else {
+      router.push("/login");
+    }
+  });
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     
+    <div className="h-screen w-screen flex items-center justify-center text-center">
+      Go to <Link href="/dashboard">Dashboard</Link>
     </div>
   );
 }
